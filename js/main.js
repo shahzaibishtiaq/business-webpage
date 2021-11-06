ea9e73baeaa61e93b435b3895d56b8b180f6dd74
Shahzaib Ishtiaq <issh5217@gmail.com>
2021-11-06 08:00:41 +0500

    Scroll Bug fixed

--- js/main.js
+++ js/main.js
@@ -8,12 +8,12 @@ $(document).ready(function(){
             $('.navbar-default').removeClass('header-fixed');
         }
     });
-     $('.back-to-top').click(function(){
-        $('html,body').animate({
-            scrollTop: 0
-        },1500,'easeInOutExpo');
-         return false;
-    });
+    //  $('.back-to-top').click(function(){
+    //     $('html,body').animate({
+    //         scrollTop: 0
+    //     },1500,'easeInOutExpo');
+    //      return false;
+    // });
     
     new WOW().init();
 });
